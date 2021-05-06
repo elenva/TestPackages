c802672c7c97af54b1117237a60a8872e1d0b0e0
elenva <282058905@qq.com>
2021-05-06 18:09:46 +0800

    a

---
+++ script/rules.js
@@ -0,0 +1,54 @@
+export default [
+  {
+    test: /\.(js|mjs|jsx|ts|tsx)$/,
+    enforce: 'pre',
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: 'eslint-loader',
+      },
+    ],
+  },
+  {
+    test: /\.(js|jsx|ts|tsx)$/,
+    // 把对.js 的文件处理交给id为happyBabel 的HappyPack 的实例执行
+    loader: 'happypack/loader?id=happyBabel',
+    exclude: /node_modules/,
+  },
+  {
+    test: /\.(css|less)$/,
+    oneOf: [
+      {
+        resourceQuery: /css_modules/, // if path include 'css_modules', will use cssModules，
+        use: cssModulesLoader.filter(Boolean),
+      },
+      {
+        use: cssLoader.filter(Boolean),
+      },
+    ],
+  },
+  {
+    test: /\.(png|jpg|gif)$/,
+    loader: 'url-loader',
+    options: {
+      limit: 8192,
+      name: 'static/media/[name].[hash:8].[ext]',
+    },
+  },
+  {
+    test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
+    use: [
+      {
+        loader: 'url-loader',
+        options: {
+          limit: '1024',
+          name: 'static/front/[name].[hash:8].[ext]',
+        },
+      },
+    ],
+  },
+  {
+    test: /\.svg$/,
+    loader: 'svg-inline-loader',
+  },
+]
---
+++ script/webpack.common.js
@@ -0,0 +1,25 @@
+const path = require('path');
+const rules = require('./rules');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: {
+    app: './src/index.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Production',
+    }),
+  ],
+  output: {
+    filename: '[name].bundle.js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules 
+  },
+  devServer: {
+    contentBase: './dist',
+  },
+};
