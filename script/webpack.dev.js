9d7f5b50927f41fb4e148930423f50e0e3d3fe2a
elenva <282058905@qq.com>
2021-05-08 11:27:10 +0800

    a

--- script/webpack.dev.js
+++ script/webpack.dev.js
@@ -6,7 +6,7 @@ module.exports = merge(common, {
   mode: 'development',
   devtool: 'inline-source-map',
   devServer: {
-    contentBase: './dist',
+    contentBase: `${process.cwd()}/dist`,
   },
   plugins: [
     new HtmlWebpackPlugin({
