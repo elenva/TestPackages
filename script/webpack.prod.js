2cb9d63355e3006df0baaddb6d70df0a47209739
Abner <abner.yuan@greenpacket.com.cn>
2021-05-10 11:44:35 +0800

    a

--- script/webpack.prod.js
+++ script/webpack.prod.js
@@ -3,4 +3,9 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
   mode: 'production',
+  output: {
+    filename: '[name].bundle.js',
+    path: path.join(process.cwd(), 'dist'),
+    clean: true,
+  },
 });
