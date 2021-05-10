eea0ce4091692fb09ef3c8f860168040134d639e
Abner <abner.yuan@greenpacket.com.cn>
2021-05-10 10:43:02 +0800

    a

--- script/webpack.common.js
+++ script/webpack.common.js
@@ -4,7 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
   entry: {
-    app: './src/index.js',
+    app: `${process.cwd()}/src/index.js`,
   },
   plugins: [
     new HtmlWebpackPlugin({
